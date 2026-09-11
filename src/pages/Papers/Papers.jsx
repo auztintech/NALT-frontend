ca3905fb91f12f3463dcd5d5618f043fd350034e
auztintech <edwinaustine712@gmail.com>
2026-09-11 12:05:58 +0100

    added paper submissions

--- src/pages/Papers/Papers.jsx
+++ src/pages/Papers/Papers.jsx
@@ -1,5 +1,6 @@
 import ConferencePapers from "../../components/ConferencePapers/ConferencePapers";
-
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 export default function Papers() {
   return (
     <div>
